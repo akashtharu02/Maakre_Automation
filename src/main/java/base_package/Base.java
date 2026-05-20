20bafa9bb28737089c2c5062de727af05df2f700
Akash Tharu <akastharu270@gmail.com>
2026-05-20 14:58:59 +0545

    Base file ti single chrome driver

--- src/main/java/base_package/Base.java
+++ src/main/java/base_package/Base.java
@@ -11,41 +11,44 @@ import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
-//public class Base {
-//    protected static WebDriver driver;
-//
-//    @BeforeMethod
-//    public void setUp() {
-//        driver = new ChromeDriver();
-//        driver.get("https://dev.tms.maakretransport.com.au/");
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-//
-//    }
 public class Base {
-   protected static WebDriver driver;
+    protected static WebDriver driver;
 
     @BeforeMethod
-    @Parameters("browser")
-    public void setUp(String browser) {
-        driver = createDriver(browser);
+    public void setUp() {
+        driver = new ChromeDriver();
         driver.get("https://dev.tms.maakretransport.com.au/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
     }
 
-    private WebDriver createDriver(String browser) {
-        switch (browser.toLowerCase()) {
-            case "chrome":
-                return new ChromeDriver();
-            case "firefox":
-                return new FirefoxDriver();
-            case "edge":
-                return new EdgeDriver();
-            default:
-                throw new IllegalArgumentException("Unsupported browser: " + browser);
-        }
-    }
+
+
+//public class Base {
+//   protected static WebDriver driver;
+//
+//    @BeforeMethod
+//    @Parameters("browser")
+//    public void setUp(String browser) {
+//        driver = createDriver(browser);
+//        driver.get("https://dev.tms.maakretransport.com.au/");
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+//    }
+//
+//    private WebDriver createDriver(String browser) {
+//        switch (browser.toLowerCase()) {
+//            case "chrome":
+//                return new ChromeDriver();
+//            case "firefox":
+//                return new FirefoxDriver();
+//            case "edge":
+//                return new EdgeDriver();
+//            default:
+//                throw new IllegalArgumentException("Unsupported browser: " + browser);
+//        }
+//    }
 
 
     @AfterMethod
