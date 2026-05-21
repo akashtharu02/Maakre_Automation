828ee977f2350d0aaa51741dd927f56307391dbb
Akash Tharu <akastharu270@gmail.com>
2026-05-21 18:41:59 +0545

    Base file sandbox added fro each broser driver

--- src/main/java/base_package/Base.java
+++ src/main/java/base_package/Base.java
@@ -4,8 +4,11 @@ package base_package;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -31,15 +34,13 @@ import java.time.Duration;
 //    }
 
 
-
 public class Base {
-   protected static WebDriver driver;
+    protected static WebDriver driver;
 
     @BeforeMethod
     @Parameters("browser")
     public void setUp(String browser) {
         driver = createDriver(browser);
-
         driver.manage().window().setSize(new Dimension(1920, 1080));
         driver.get("https://dev.tms.maakretransport.com.au/");
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
@@ -47,17 +48,35 @@ public class Base {
 
     private WebDriver createDriver(String browser) {
         switch (browser.toLowerCase()) {
+
             case "chrome":
-                return new ChromeDriver();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                chromeOptions.addArguments("--disable-gpu");
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                return new ChromeDriver(chromeOptions);
+
             case "firefox":
-                return new FirefoxDriver();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--no-sandbox");
+                firefoxOptions.addArguments("--disable-dev-shm-usage");
+                firefoxOptions.addArguments("--width=1920");
+                firefoxOptions.addArguments("--height=1080");
+                return new FirefoxDriver(firefoxOptions);
+
             case "edge":
-                return new EdgeDriver();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--no-sandbox");
+                edgeOptions.addArguments("--disable-dev-shm-usage");
+                edgeOptions.addArguments("--disable-gpu");
+                edgeOptions.addArguments("--remote-allow-origins=*");
+                return new EdgeDriver(edgeOptions);
+
             default:
                 throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
     }
-
 
     @AfterMethod
     public void tearDown() throws InterruptedException {
