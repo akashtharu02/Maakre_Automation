eb3422005204fa9fb02698c6a6641f6f0aa32b3b
Akash Tharu <akastharu270@gmail.com>
2026-05-21 11:33:31 +0545

    element clickable to visibility of the element

--- src/main/java/pages_package/DashboardPage.java
+++ src/main/java/pages_package/DashboardPage.java
@@ -11,52 +11,58 @@ import java.time.Duration;
 public class DashboardPage {
     private final WebDriver driver;
     private final WebDriverWait wait;
-    public DashboardPage(WebDriver driver){
-        this.driver =driver;
+
+    public DashboardPage(WebDriver driver) {
+        this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
-   private final By receiverTab = By.xpath("//span[text()='Receiver']");
-   private final By profilee = By.xpath("//h4[@class='text-sm font-medium text-gray-800 truncate']");
-   private final By settingsTab = By.xpath("//span[text()='Settings']");
-   private final By consignmentsDropdown = By.xpath("//span[text()=\"Consignments\"]");
-   private final By viewConsignmentsTab = By.xpath("//a[text()=\"View Consignments\"]");
+
+    private final By receiverTab = By.xpath("//span[text()='Receiver']");
+    private final By profilee = By.xpath("//h4[@class='text-sm font-medium text-gray-800 truncate']");
+    private final By settingsTab = By.xpath("//span[text()='Settings']");
+    private final By consignmentsDropdown = By.xpath("//span[text()=\"Consignments\"]");
+    private final By viewConsignmentsTab = By.xpath("//a[text()=\"View Consignments\"]");
     private final By logoutTab = By.xpath("//nav/ul/div/span");
 
 
-    public void clickReceiver(){
+    public void clickReceiver() {
         WebElement element = wait.until(
-                ExpectedConditions.elementToBeClickable(receiverTab)
-        );
-       element.click();
-    }
-    public void clickOnProfile(){
-        WebElement element = wait.until(
-                ExpectedConditions.elementToBeClickable(profilee)
-        );
-        element.click();
-    }
-    public void clickOnSettingsTab(){
-        WebElement element = wait.until(
-                ExpectedConditions.elementToBeClickable(settingsTab)
-        );
-        element.click();
-    }
-    public void clickConsignmentsDropdown(){
-        WebElement element = wait.until(
-                ExpectedConditions.elementToBeClickable(consignmentsDropdown)
-        );
-        element.click();
-    }
-    public void  clickViewConsignments(){
-        WebElement element = wait.until(
-                ExpectedConditions.elementToBeClickable(viewConsignmentsTab)
+                ExpectedConditions.visibilityOfElementLocated(receiverTab)
         );
         element.click();
     }
 
-    public void clickOnLogoutTab(){
+    public void clickOnProfile() {
         WebElement element = wait.until(
-                ExpectedConditions.elementToBeClickable(logoutTab)
+                ExpectedConditions.visibilityOfElementLocated(profilee)
+        );
+        element.click();
+    }
+
+    public void clickOnSettingsTab() {
+        WebElement element = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(settingsTab)
+        );
+        element.click();
+    }
+
+    public void clickConsignmentsDropdown() {
+        WebElement element = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(consignmentsDropdown)
+        );
+        element.click();
+    }
+
+    public void clickViewConsignments() {
+        WebElement element = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(viewConsignmentsTab)
+        );
+        element.click();
+    }
+
+    public void clickOnLogoutTab() {
+        WebElement element = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(logoutTab)
         );
         element.click();
     }
