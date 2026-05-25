d584454a744d683440b41986cd29481910ebab65
Akash Tharu <akastharu270@gmail.com>
2026-05-25 10:35:17 +0545

    Change in Base file

--- src/main/java/base_package/Base.java
+++ src/main/java/base_package/Base.java
@@ -15,69 +15,69 @@ import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
-//public class Base {
-//    protected static WebDriver driver;
-//
-//    @BeforeMethod
-//    public void setUp() {
-//        driver = new ChromeDriver();
-//
-//        driver.manage().window().setSize(new Dimension(1920, 1080));
-//        driver.get("https://dev.tms.maakretransport.com.au/");
-//
-//
-//        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
-//
-//
-//
-//    }
-
-
 public class Base {
     protected static WebDriver driver;
 
     @BeforeMethod
-    @Parameters("browser")
-    public void setUp(String browser) {
-        driver = createDriver(browser);
+    public void setUp() {
+        driver = new ChromeDriver();
+
         driver.manage().window().setSize(new Dimension(1920, 1080));
         driver.get("https://dev.tms.maakretransport.com.au/");
+
+
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+
+
+
     }
 
-    private WebDriver createDriver(String browser) {
-        switch (browser.toLowerCase()) {
-
-            case "chrome":
-                ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--no-sandbox");
-                chromeOptions.addArguments("--disable-dev-shm-usage");
-                chromeOptions.addArguments("--disable-gpu");
-                chromeOptions.addArguments("--remote-allow-origins=*");
-                return new ChromeDriver(chromeOptions);
-
-            case "firefox":
-                FirefoxOptions firefoxOptions = new FirefoxOptions();
-                firefoxOptions.addArguments("--headless");
-                firefoxOptions.addArguments("--no-sandbox");
-                firefoxOptions.addArguments("--disable-dev-shm-usage");
-                firefoxOptions.addArguments("--width=1920");
-                firefoxOptions.addArguments("--height=1080");
-                return new FirefoxDriver(firefoxOptions);
-
-            case "edge":
-                EdgeOptions edgeOptions = new EdgeOptions();
-                edgeOptions.addArguments("--headless");
-                edgeOptions.addArguments("--no-sandbox");
-                edgeOptions.addArguments("--disable-dev-shm-usage");
-                edgeOptions.addArguments("--disable-gpu");
-                edgeOptions.addArguments("--remote-allow-origins=*");
-                return new EdgeDriver(edgeOptions);
-
-            default:
-                throw new IllegalArgumentException("Unsupported browser: " + browser);
-        }
-    }
+//
+//public class Base {
+//    protected static WebDriver driver;
+//
+//    @BeforeMethod
+//    @Parameters("browser")
+//    public void setUp(String browser) {
+//        driver = createDriver(browser);
+//        driver.manage().window().setSize(new Dimension(1920, 1080));
+//        driver.get("https://dev.tms.maakretransport.com.au/");
+//        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+//    }
+//
+//    private WebDriver createDriver(String browser) {
+//        switch (browser.toLowerCase()) {
+//
+//            case "chrome":
+//                ChromeOptions chromeOptions = new ChromeOptions();
+//                chromeOptions.addArguments("--no-sandbox");
+//                chromeOptions.addArguments("--disable-dev-shm-usage");
+//                chromeOptions.addArguments("--disable-gpu");
+//                chromeOptions.addArguments("--remote-allow-origins=*");
+//                return new ChromeDriver(chromeOptions);
+//
+//            case "firefox":
+//                FirefoxOptions firefoxOptions = new FirefoxOptions();
+//                firefoxOptions.addArguments("--headless");
+//                firefoxOptions.addArguments("--no-sandbox");
+//                firefoxOptions.addArguments("--disable-dev-shm-usage");
+//                firefoxOptions.addArguments("--width=1920");
+//                firefoxOptions.addArguments("--height=1080");
+//                return new FirefoxDriver(firefoxOptions);
+//
+//            case "edge":
+//                EdgeOptions edgeOptions = new EdgeOptions();
+//                edgeOptions.addArguments("--headless");
+//                edgeOptions.addArguments("--no-sandbox");
+//                edgeOptions.addArguments("--disable-dev-shm-usage");
+//                edgeOptions.addArguments("--disable-gpu");
+//                edgeOptions.addArguments("--remote-allow-origins=*");
+//                return new EdgeDriver(edgeOptions);
+//
+//            default:
+//                throw new IllegalArgumentException("Unsupported browser: " + browser);
+//        }
+//    }
 
     @AfterMethod
     public void tearDown() throws InterruptedException {
